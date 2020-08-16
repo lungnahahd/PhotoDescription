14533263a9fc31ec1305bd21035669a7567e3c9a
lungnahahd <jyoungjk33@gmail.com>
2020-08-16 22:49:34 +0900

    사진 정리 앱

---
+++ app/src/main/java/com/evanandroid/apps/photodescription/SplashScreen.kt
@@ -0,0 +1,19 @@
+package com.evanandroid.apps.photodescription
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(baseContext,MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        },2000)
+    }
+}
