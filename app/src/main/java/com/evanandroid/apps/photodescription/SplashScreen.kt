ca6a24b261367db9438b2dac251dd99e1dac4f45
lungnahahd <jyoungjk33@gmail.com>
2020-08-18 07:22:51 +0900

    네비 바 기능 구현

--- app/src/main/java/com/evanandroid/apps/photodescription/SplashScreen.kt
+++ app/src/main/java/com/evanandroid/apps/photodescription/SplashScreen.kt
@@ -11,7 +11,7 @@ class SplashScreen : AppCompatActivity() {
         setContentView(R.layout.activity_splash_screen)
         val handler = Handler()
         handler.postDelayed({
-            val intent = Intent(baseContext,MainActivity::class.java)
+            val intent = Intent(baseContext,MainActivity2::class.java)
             startActivity(intent)
             finish()
         },2000)
